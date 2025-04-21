9ae928f7d030a772e6febe82a7669b1d505a8b38
mungyun <mungyun1234@naver.com>
2025-04-21 20:32:55 +0900

    Feat: 초기 폴더 및 의존성 추가

---
+++ src/api/axios.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const proxy = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
